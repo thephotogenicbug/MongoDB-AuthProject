ff2b1eb7d377df2b668d8c32e26cccd013a1b192
thephotogenicbug <navinkumarachari@gmail.com>
2024-11-29 13:42:58 +0530

    [MongoDb Auth] : create user model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,16 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// db connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server" });
